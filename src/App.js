36b67b8579f81c734fa1c3c8707ee60bc741d977
commude-app <thuongdq@gmail.com>
2025-11-05 17:26:16 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './page/Home';
+import Products from './page/Products';
+import Profile from './page/Profile';
+import Contact from './page/Contact';
+import Header from './layout/Header';
+import "./scss/main.scss"
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+      <Header />
+        <Routes>
+          <Route path="/" element={Home} />
+          <Route path="/products" element={Products} />
+          <Route path="/profile" element={Profile} />
+          <Route path="/contact" element={Contact} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
---
+++ src/layout/Header.jsx
@@ -0,0 +1,71 @@
+import { Link } from 'react-router-dom';
+import { FaShoppingCart, FaBars } from "react-icons/fa";
+import { FaTimes } from "react-icons/fa";
+import { MdOutlineSupportAgent, MdOutlineNavigateNext } from "react-icons/md";
+import "../scss/layouts/header.scss"
+import { useState } from 'react';
+export default function Header() {
+    const [open, setOpen] = useState(false);
+    return (
+        <>
+            <div className="header__container">
+                <div className="header__logo"><img src="/images/logo.svg" alt="logo" /></div>
+                <div className="header__content">
+                    <div className="header__box">
+                        <button onClick={() => setOpen(true)} className="header__container-bar">
+                            <FaBars className="header__bar" />
+                        </button>
+                        <div className="header__page" >
+                            <Link to='/'>Trang chủ</Link>
+                            <Link to='/product'>Sản phẩm</Link>
+                            <Link to='/profile'>Hồ sơ</Link>
+                            <Link to='/contact'>Liên hệ</Link>
+                        </div>
+                        <div className={`header__page-fixed ${open ? "open-mobie" : ""} `} >
+                            <div className="header-fixed__top">
+                                <div className="header__logo"><img src="/images/logo.svg" alt="logo" /></div>
+                                <button className='header-fixed__close' onClick={() => setOpen(false)}>
+                                    <FaTimes className='header-fixed__close-icon' />
+                                </button>
+                            </div>
+                            <Link className="header-fixed__link" to='/'>Trang chủ
+                                <div className="header-fixed__link-icon">
+                                    <MdOutlineNavigateNext />
+                                </div>
+                            </Link>
+                            <Link className="header-fixed__link" to='/product'>Sản phẩm
+                                <div className="header-fixed__link-icon">
+                                    <MdOutlineNavigateNext />
+                                </div>
+                            </Link>
+                            <Link className="header-fixed__link" to='/profile'>Hồ sơ
+                                <div className="header-fixed__link-icon">
+                                    <MdOutlineNavigateNext />
+                                </div>
+                            </Link>
+                            <Link className="header-fixed__link" to='/contact'>Liên hệ
+                                <div className="header-fixed__link-icon">
+                                    <MdOutlineNavigateNext />
+                                </div>
+                            </Link>
+                        </div>
+                    </div>
+                    <div className="header__change">
+                        <div className="change__box chang__shopping">
+                            <FaShoppingCart className='change__icon' />
+                            <div className="chage__text">
+                                Mua ngay
+                            </div>
+                        </div>
+                        <div className="change__box pc">
+                            <MdOutlineSupportAgent className='change__icon' />
+                            <div className="change__tex">
+                                Tư vấn
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
